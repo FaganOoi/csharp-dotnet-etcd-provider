aa4bfd23fb4ac06f92a55e8d93203cd5be352ecc
FaganDes <keong07155@gmail.com>
2022-07-31 16:05:40 +0800

    refactor(xunit): Simplify the code and remove unused settings/code

---
+++ DotnetEtcdProvider.Test/Models/TestModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DotnetEtcdProvider.Test.Models;
+
+public class TestModel
+{
+    public int IntegerValue { get; set; }
+
+    public string StringValue { get; set; }
+
+    public double DoubleValue { get; set; }
+
+    public string[] StringArrayValue { get; set; }
+
+    public double[] DoubleArrayValue { get; set; }
+
+    public TestObject ObjectModel { get; set; }
+}
+
+public class TestObject
+{
+    public string MyProperty { get; set; }
+
+    public decimal DecimalValue { get; set; }
+}
--- DotnetEtcdProvider.Test/UnitTest.cs
+++ DotnetEtcdProvider.Test/UnitTest.cs
@@ -1,6 +1,8 @@
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
 using dotnet_etcd;
+using DotnetEtcdProvider.Extensions;
+using DotnetEtcdProvider.Test.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,25 +16,23 @@ public class UnitTest : IAsyncLifetime
         .WithImage("bitnami/etcd:latest")
         .WithName("bitnami-etcd-test")
         .WithEnvironment("ALLOW_NONE_AUTHENTICATION", "yes")
-        .WithEnvironment("ETCD_ADVERTISE_CLIENT_URLS", "http://etcd-server:2379")
+        // 2379: Client Request
         .WithPortBinding(2379)
         .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(2379))
-        .WithPortBinding(2380)
-        .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(2380))
         .Build();
 
-    private DotnetEtcdProviderTest _dotnetEtcdProviderTest = new();
+    private TestModel _dotnetEtcdProviderTest = new();
 
     public UnitTest()
     {
-        _dotnetEtcdProviderTest = new DotnetEtcdProviderTest
+        _dotnetEtcdProviderTest = new TestModel
         {
             IntegerValue = 1,
             DoubleValue = 2.1,
             DoubleArrayValue = new double[] { 2.2, 2.3 },
             StringValue = "msg",
             StringArrayValue = new string[] { "String02", "String03" },
-            ObjectModel = new DotnetEtcdProviderTestObject
+            ObjectModel = new TestObject
             {
                 MyProperty = "Property From Etcd",
                 DecimalValue = 3.01m
@@ -51,15 +51,11 @@ public class UnitTest : IAsyncLifetime
         await this.testcontainer.StartAsync();
 
         EtcdClient etcdClient = new EtcdClient("http://localhost:2379");
-        await etcdClient.PutAsync("DotnetEtcdProviderTest:IntegerValue", _dotnetEtcdProviderTest.IntegerValue.ToString());
-        await etcdClient.PutAsync("DotnetEtcdProviderTest:DoubleValue", _dotnetEtcdProviderTest.DoubleValue.ToString());
-        await etcdClient.PutAsync("DotnetEtcdProviderTest:DoubleArrayValue:0", _dotnetEtcdProviderTest.DoubleArrayValue[0].ToString());
-        await etcdClient.PutAsync("DotnetEtcdProviderTest:DoubleArrayValue:1", _dotnetEtcdProviderTest.DoubleArrayValue[1].ToString());
-        await etcdClient.PutAsync("DotnetEtcdProviderTest:StringValue", _dotnetEtcdProviderTest.StringValue);
-        await etcdClient.PutAsync("DotnetEtcdProviderTest:StringArrayValue:0", _dotnetEtcdProviderTest.StringArrayValue[0]);
-        await etcdClient.PutAsync("DotnetEtcdProviderTest:StringArrayValue:1", _dotnetEtcdProviderTest.StringArrayValue[1]);
-        await etcdClient.PutAsync("DotnetEtcdProviderTest:ObjectModel:MyProperty", _dotnetEtcdProviderTest.ObjectModel.MyProperty);
-        await etcdClient.PutAsync("DotnetEtcdProviderTest:ObjectModel:DecimalValue", _dotnetEtcdProviderTest.ObjectModel.DecimalValue.ToString());
+        Dictionary<string, string> keyValuePairs = GetReflectionModel(_dotnetEtcdProviderTest, "");
+        foreach (var keyPair in keyValuePairs)
+        {
+            await etcdClient.PutAsync(keyPair.Key, keyPair.Value);
+        }
 
     }
 
@@ -76,11 +72,11 @@ public class UnitTest : IAsyncLifetime
                 configuration.AddEtcdConfiguration("Etcd");
             })
             .ConfigureServices((context, services) =>
-                services.Configure<DotnetEtcdProviderTest>(
-                    context.Configuration.GetSection("DotnetEtcdProviderTest")))
+                services.Configure<TestModel>(
+                    context.Configuration.GetSection("TestModel")))
             .Build();
 
-        var options = host.Services.GetRequiredService<IOptions<DotnetEtcdProviderTest>>().Value;
+        var options = host.Services.GetRequiredService<IOptions<TestModel>>().Value;
 
         Assert.Equal(_dotnetEtcdProviderTest.IntegerValue, options.IntegerValue);
         Assert.Equal(_dotnetEtcdProviderTest.DoubleValue, options.DoubleValue);
@@ -89,26 +85,74 @@ public class UnitTest : IAsyncLifetime
         Assert.Equal(_dotnetEtcdProviderTest.ObjectModel.MyProperty, options.ObjectModel.MyProperty);
         Assert.Equal(_dotnetEtcdProviderTest.ObjectModel.DecimalValue, options.ObjectModel.DecimalValue);
     }
-}
 
-public class DotnetEtcdProviderTest
-{
-    public int IntegerValue { get; set; }
+    private Dictionary<string, string> GetReflectionModel(object model, string parentName, string currentName = "")
+    {
+        Dictionary<string, string> keyValuePairs = new();
+        string dataModelName = "";
+        if (parentName.HasData())
+        {
 
-    public string StringValue { get; set; }
+            dataModelName = parentName + ":" + currentName;
+        }
+        else
+        {
+            dataModelName += model.GetType().Name;
+        }
 
-    public double DoubleValue { get; set; }
+        var propertiesInfo = model.GetType().GetProperties();
+        foreach (var propertyInfo in propertiesInfo)
+        {
+            if (propertyInfo.PropertyType.IsValueType || propertyInfo.PropertyType == typeof(String))
+            {
+                keyValuePairs.Add(
+                    $"{dataModelName}:{propertyInfo.Name}",
+                    propertyInfo.GetValue(model).ToString()
+                );
+            }
+            else if (propertyInfo.PropertyType.IsValueType && propertyInfo.PropertyType.IsArray)
+            {
+                Array a = (Array)propertyInfo.GetValue(model);
+                for (int i = 0; i < a.Length; i++)
+                {
+                    keyValuePairs.Add(
+                        $"{dataModelName}:{propertyInfo.Name}:{i}",
+                        a.GetValue(i).ToString()
+                    );
+                }
+            }
+            else if (propertyInfo.PropertyType.IsClass && propertyInfo.PropertyType.IsArray)
+            {
+                Array a = (Array)propertyInfo.GetValue(model);
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a.GetValue(i).GetType().IsValueType || a.GetValue(i).GetType() == typeof(String))
+                    {
+                        keyValuePairs.Add(
+                            $"{dataModelName}:{propertyInfo.Name}:{i}",
+                            a.GetValue(i).ToString()
+                        );
+                    }
+                    else
+                    {
+                        var dat = GetReflectionModel(a.GetValue(i), dataModelName, propertyInfo.Name);
+                        foreach (var tmp in dat)
+                        {
+                            keyValuePairs.Add(tmp.Key, tmp.Value);
+                        }
+                    }
+                }
+            }
+            else if (propertyInfo.PropertyType.IsClass)
+            {
+                var dat = GetReflectionModel(propertyInfo.GetValue(model), dataModelName, propertyInfo.Name);
+                foreach (var tmp in dat)
+                {
+                    keyValuePairs.Add(tmp.Key, tmp.Value);
+                }
+            }
+        }
 
-    public string[] StringArrayValue { get; set; }
-
-    public double[] DoubleArrayValue { get; set; }
-
-    public DotnetEtcdProviderTestObject ObjectModel { get; set; }
-}
-
-public class DotnetEtcdProviderTestObject
-{
-    public string MyProperty { get; set; }
-
-    public decimal DecimalValue { get; set; }
+        return keyValuePairs;
+    }
 }
